1a517001665b206cd816ddc5e75f82f0867513c5
stefanialukaszewska <stefania.lukaszewska@gmail.com>
2025-03-19 17:30:33 +0100

    basic password generator, not finished

--- Passwords/src/main/java/Generator.java
+++ Passwords/src/main/java/Generator.java
@@ -1,11 +1,11 @@
 public class Generator {
-    public static String GeneratePassword(){
-        String password= "";
-        for (int i = 0; i < 20; i++) {
+    public static String GeneratePassword(int length){
+        StringBuilder password= new StringBuilder();
+        for (int i = 0; i < length; i++) {
             int j = 33 + (int)(Math.random()*94);
             char c = (char)j;
-            password+=c;
+            password.append(c);
         }
-        return password;
+        return password.toString();
     }
 }
